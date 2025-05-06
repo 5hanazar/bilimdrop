3f678fff0f3f057177b3e887b273c55c60d8da17
Shanazar <newshanazar@gmail.com>
2025-05-06 20:58:54 +0500

    Add project files.

---
+++ Bilim Drop/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Bilim_Drop
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Bilim Drop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Bilim_Drop
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        [STAThread]
+        static void Main()
+        {
+            bool createdNew = true;
+            using (System.Threading.Mutex mutex = new System.Threading.Mutex(true, Application.ProductName, out createdNew))
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainForm());
+                }
+                else
+                {
+                    Process current = Process.GetCurrentProcess();
+                    foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+                    {
+                        if (process.Id != current.Id) { SetForegroundWindow(process.MainWindowHandle); break; }
+                    }
+                }
+            }
+        }
+    }
+}
